cd99667cfd989bbeee69a5dda6d4e6d52d263014
lookaiz <quezacolor@gmail.com>
2015-09-04 23:40:21 +0200

    3- La descente
    3- La descente

---
+++ src/main/java/codingame/LaDescente.java
@@ -0,0 +1,27 @@
+package codingame;
+
+import java.util.Scanner;
+
+public class LaDescente {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int spaceX = in.nextInt();
+            in.nextInt();
+
+            int max = -1;
+            int idx = -1;
+            for (int i = 0; i < 8; i++) {
+                int m = in.nextInt();
+                if (m > max) {
+                    max = m;
+                    idx = i;
+                }
+            }
+
+            System.out.println((spaceX == idx) ? "FIRE" : "HOLD");
+        }
+    }
+}
