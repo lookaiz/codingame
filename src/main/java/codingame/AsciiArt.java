5b2455c6fd1789b4d908f44368058493591eaa3c
lookaiz <quezacolor@gmail.com>
2015-09-06 21:38:14 +0200

    Ascii Art

---
+++ src/main/java/codingame/AsciiArt.java
@@ -0,0 +1,38 @@
+package codingame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AsciiArt {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int l = in.nextInt(); // length
+        in.nextLine();
+        int h = in.nextInt(); // height
+        in.nextLine();
+        String t = in.nextLine(); // text
+
+        List<String> rows = new ArrayList<>();
+        for (int i = 0; i < h; i++) {
+            rows.add(in.nextLine());
+        }
+
+        for (int hh = 0; hh < h; hh++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < t.length(); j++) {
+                int pos = getCharIndex(Character.toUpperCase(t.charAt(j)));
+                sb.append(rows.get(hh).substring((pos - 1) * l, (pos - 1) * l + l));
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static int getCharIndex(char letter) {
+        if (letter >= 'A' && letter <= 'Z') {
+            return (int) letter - 'A' + 1;
+        }
+        return 27;
+    }
+}
