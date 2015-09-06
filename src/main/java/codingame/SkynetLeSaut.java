268aa41055e94a74fceaeab8c3dbe605b495c7f5
lookaiz <quezacolor@gmail.com>
2015-09-06 20:32:27 +0200

    Skynet - Le saut

---
+++ src/main/java/codingame/SkynetLeSaut.java
@@ -0,0 +1,37 @@
+package codingame;
+
+import java.util.Scanner;
+
+public class SkynetLeSaut {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int road = in.nextInt(); // the length of the road before the gap.
+        int gap = in.nextInt(); // the length of the gap.
+        int platform = in.nextInt(); // the length of the landing platform.
+
+        // game loop
+        while (true) {
+            int speed = in.nextInt(); // the motorbike's speed.
+            int coordX = in.nextInt(); // the position on the road of the motorbike.
+
+            if ((coordX + 1) == road) {
+                // Just before the gap
+                System.out.println("JUMP");
+            }
+            else if (gap < (speed - 1)) {
+                System.out.println("SLOW");
+            }
+            else if (coordX >= (road + gap)) {
+                // After the gap
+                System.out.println("SLOW");
+            }
+            else if (gap == (speed - 1)) {
+                System.out.println("WAIT");
+            }
+            else {
+                System.out.println("SPEED");
+            }
+        }
+    }
+}
